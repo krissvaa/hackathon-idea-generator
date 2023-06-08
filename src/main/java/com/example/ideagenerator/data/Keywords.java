84bd622633d2ec365679274757304761e2ed3224
Kriss Seglins <101052651+krissvaa@users.noreply.github.com>
2023-06-08 14:24:46 +0300

    Add first keywords

--- src/main/java/com/example/ideagenerator/data/Keywords.java
+++ src/main/java/com/example/ideagenerator/data/Keywords.java
@@ -4,9 +4,44 @@ import java.util.List;
 
 public class Keywords {
 
-    public static final List<String> PRODUCTS = List.of("Mobile Apps", "Robot");
-    public static final List<String> SUBJECTS = List.of("Human Speech", "Laundry Folding");
-    public static final List<String> HOWS = List.of("Offer survival tips");
-    public static final List<String> RESULTS = List.of("Dance Moves", "Masterpieces with", "Support and counseling");
+    public static final List<String> PRODUCTS = List.of("Task Management", "Humor", "Calendar Invites",
+            "Content", "Framework", "Vaadin", "Placeholder Text", "Vaadin Flow", "Prompt Generator", "Topic",
+            "Social Interaction", "Community", "Outfit Suggestions", "Relationship Management", "Text Messages",
+            "Automation", "Virtual Assistant", "Robotic Process Automation", "Chatbot", "Augmented Reality",
+            "Media Monitoring", "Weekly Calls", "Simpsons", "Birthday Reminder", "Web Component", "Cryptocurrency",
+            "Personal Coin", "Solar Power", "Stock Trading Bot", "Gmail");
+    public static final List<String> SUBJECTS = List.of("Productivity","Entertainment","Scheduling",
+            "Development","Software Development", "Web Development","User Experience",
+            "Web Development","Content Creation","Assistance","Community","Engagement",
+            "Fashion","Communication","Efficiency","Personal Assistance",
+            "Repetitive Tasks","Conversational AI","Immersive Technology","Data Analysis",
+            "Communication","Entertainment","Personal Organization","Web Development",
+            "Financial Technology","Personal Finance","Renewable Energy","Finance","Email");
+    public static final List<String> HOWS = List.of(
+            "Task management","Humor","Calendar invites","Content creation","Framework","Vaadin",
+            "Placeholder text","Vaadin Flow","Prompt generator","Topic","Social interaction",
+            "Community","Outfit suggestions","Relationship management","Text messages","Automation",
+            "Virtual assistant","Robotic process automation","Chatbot","Augmented reality",
+            "Media monitoring","Weekly calls","Simpsons","Birthday reminder","Web component",
+            "Cryptocurrency","Personal coin","Solar power","Stock trading bot","Gmail",
+            "deadlines","jokes","meeting scheduling","writing","software development",
+            "web development","UI design","web development","content creation","subject",
+            "community","social platform","fashion","communication","SMS","process optimization",
+            "personal tasks","repetitive tasks","texting","AR","news tracking","meetings","TV show",
+            "automated text messages","web development","trading","cryptocurrency","renewable energy",
+            "automated trading","email","priorities","laughter","event management","programming",
+            "libraries","user interface","user experience","technology compatibility","writing prompts",
+            "content","engagement","user interaction","clothing","text messages","chat","task automation",
+            "automation","efficiency","conversational interface","virtual reality","social media analysis",
+            "quotes","cartoon","organization","coding","blockchain","investment","sustainability","tweets",
+            "sensitive data","task assignment","funny content","programming language","tools","frontend",
+            "form fields","discussion","networking","communication","styling","instant messaging","efficiency",
+            "deception","API","agenda","humor","challenge","trading bot","finance","Raspberry Pi","fintech",
+            "email security","discussion"
+    );
+    public static final List<String> RESULTS = List.of("Procrastination","Programming Language","Tips",
+            "Hilla","Vaadin Docs Assistant","Prompts","Weather API","Automation",
+            "Deception","SMS Alerts","Quotes","Automated Text Messages","Challenge",
+            "Trading Bot","Cryptocurrency","Raspberry Pi","Tweets");
 
 }
